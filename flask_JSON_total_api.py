5ace8b943275c8e6231e1665ac46a4f337d50288
Javed793 <74866164+Javed793@users.noreply.github.com>
2020-11-22 17:11:25 +0000

    Update flask_JSON_total_api.py

--- flask_JSON_total_api.py
+++ flask_JSON_total_api.py
@@ -13,15 +13,13 @@
 
 from flask import Flask
 import json
-import random #added a little bit of flair, uncomment lines with #DEL and comment with #COMM to return to no flair ;)
+import random 
 
 app = Flask(__name__)
 
 @app.route('/total')
 def total_endpoint():
     #numbers_to_add = list(range(10000001)) #why use such a large sample of numbers????
-
-
     #numbers_to_add = [27, 314, 88119, 1, 43, 1337, 9009] #DEL
     numbers_to_add = random.sample(range(99999), 10) #COMM
 
